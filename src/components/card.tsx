9faf64e359b46419cbca6cebbcafd69b44b513cb
Jan Gosmann <jan@hyper-world.de>
2021-10-31 17:52:37 +0100

    Fetch start page cards from Contentful

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -7,16 +7,13 @@ export interface CardProps {
   children: React.ReactNode
   download?: string
   link?: string
+  video?: string
 }
 
-const Card = ({ title, children, download, link }: CardProps) => (
+const Card = ({ title, video, children, download, link }: CardProps) => (
   <div className="rounded shadow w-64 flex flex-col">
     <h4 className="text-primary-700 m-2">{title}</h4>
-    <StaticImage
-      src="../images/thumb-placeholder.png"
-      alt="video"
-      className="w-full shadow"
-    />
+    {video && <video controls src={video} />}
     <div className="prose leading-snug text-sm m-2 flex-grow">{children}</div>
     <div className="flex flex-row flex-wrap justify-between m-2 mt-2">
       <div>{download && <a href={download}>Download</a>}</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,72 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Card from "../components/card"
 import SloganCarousel from "../components/sloganCarousel"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <div className="text-sm font-normal text-gray-600">
-      Sie sind hier: Startseite
-    </div>
-    <SloganCarousel />
-    <div class="prose my-8 mx-auto">
-      <p>
-        Herzlich Willkommen auf der Seite des Arbeitskreises von Annette Marohn!
-      </p>
-      <p>
-        Hier finden Sie neue Unterrichtskonzepte mit Material, welches Sie
-        kostenlos herunterladen und nutzen können.
-      </p>
-      <p>Wer wir sind und was diese Website soll erfahren Sie TODO.</p>
-    </div>
-    <div className="flex flex-wrap gap-8 m-8 justify-center">
-      <Card title="choice²learn">
-        <ul>
-          <li>Schülervorstellungen</li>
-          <li>
-            Weg der naturwissenschaftlichen Erkenntnisgewinnung durch
-            Falsifikation
-          </li>
-          <li>Von der Grundschule bis in die Sek II</li>
-          <li>Für hetoregene Lerngruppen</li>
-        </ul>
-      </Card>
-      <Card
-        title={
-          <StaticImage
-            src="../images/chemlevel.png"
-            alt="chem Level"
-            height={24}
-          />
+const IndexPage = () => {
+  const conceptPages = useStaticQuery(graphql`
+    query ConceptPagesQuery {
+      allContentfulConceptPage {
+        nodes {
+          title
+          titleImage {
+            gatsbyImageData(layout: CONSTRAINED, height: 24)
+          }
+          shortDescription {
+            raw
+          }
+          video: childContentfulConceptPageShortVideoJsonNode {
+            secure_url
+          }
         }
-      >
-        <ul>
-          <li>Sprachsensibler Unterricht</li>
-          <li>Johnestone-Dreieck</li>
-          <li>Differenzierung</li>
-          <li>Analog und Digital</li>
-          <li>Für hetoregene Lerngruppen</li>
-        </ul>
-      </Card>
-      <Card title="One more card">Description</Card>
-    </div>
-  </Layout>
-)
+      }
+    }
+  `).allContentfulConceptPage.nodes
+
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div className="text-sm font-normal text-gray-600">
+        Sie sind hier: Startseite
+      </div>
+      <SloganCarousel />
+      <div class="prose my-8 mx-auto">
+        <p>
+          Herzlich Willkommen auf der Seite des Arbeitskreises von Annette
+          Marohn!
+        </p>
+        <p>
+          Hier finden Sie neue Unterrichtskonzepte mit Material, welches Sie
+          kostenlos herunterladen und nutzen können.
+        </p>
+        <p>Wer wir sind und was diese Website soll erfahren Sie TODO.</p>
+      </div>
+      <div className="flex flex-wrap gap-8 m-8 justify-center">
+        {conceptPages.map(conceptPage => {
+          const titleImage = conceptPage.titleImage && (
+            <GatsbyImage
+              image={conceptPage.titleImage.gatsbyImageData}
+              alt={conceptPage.title}
+            />
+          )
+          return (
+            <Card
+              title={titleImage || conceptPage.title}
+              video={conceptPage.video.secure_url}
+            >
+              {conceptPage.shortDescription &&
+                renderRichText(conceptPage.shortDescription)}
+            </Card>
+          )
+        })}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
