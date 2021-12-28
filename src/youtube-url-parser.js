c9f1ebcde057bdab7ac07b7ed1752a84e79c0375
Jan Gosmann <jan@hyper-world.de>
2021-12-28 14:41:48 +0100

    Test additional YouTube URLs
    (And anchor URL at string start)

--- src/youtube-url-parser.js
+++ src/youtube-url-parser.js
@@ -1,8 +1,8 @@
 const extractVideoId = url => {
   const m1 = url.match(
-    /(https?:\/\/)?(.*\.)?youtube(-nocookie)?\.com\/watch\?.*v=([_a-zA-Z0-9]+)/
+    /^(https?:\/\/)?(.*\.)?youtube(-nocookie)?\.com\/watch\?.*v=([_a-zA-Z0-9]+)/
   )
-  const m2 = url.match(/(https?:\/\/)?(.*\.)?youtu\.be\/([_a-zA-Z0-9]+)/)
+  const m2 = url.match(/^(https?:\/\/)?(.*\.)?youtu\.be\/([_a-zA-Z0-9]+)/)
   const videoId = (m1 && m1[4]) || (m2 && m2[3]) || url
   return videoId
 }
--- src/youtube-url-parser.spec.ts
+++ src/youtube-url-parser.spec.ts
@@ -9,6 +9,10 @@ describe("youtube-url-parser", () =>
     "https://youtube-nocookie.com/watch?v=IdkCEioCp24",
     "https://www.youtube-nocookie.com/watch?v=IdkCEioCp24",
     "https://www.youtube.com/watch?foo=bar&v=IdkCEioCp24&baz=xyz",
+    "https://youtu.be/IdkCEioCp24",
+    "http://youtu.be/IdkCEioCp24",
+    "https://www.youtu.be/IdkCEioCp24",
+    "https://youtu.be/IdkCEioCp24&t=26",
   ].forEach(url => {
     it(`extracts video ID from ${url}`, () => {
       expect(extractVideoId(url)).toEqual("IdkCEioCp24")
