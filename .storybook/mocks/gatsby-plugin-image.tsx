8fa07158cc2e16ad18fe7e6720e0839256ac8787
Jan Gosmann <jan@hyper-world.de>
2022-01-22 12:46:09 +0100

    Fix eslint configuration for React

--- .storybook/mocks/gatsby-plugin-image.tsx
+++ .storybook/mocks/gatsby-plugin-image.tsx
@@ -1,4 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const React = require("react")
+
+function StaticImage({ src }: { src: string }) {
+  return <div>StaticImage: {src}</div>
+}
+
 module.exports = require("gatsby-plugin-image-original")
-module.exports.StaticImage = ({ src }: { src: string }) => (
-  <div>StaticImage: {src}</div>
-)
+module.exports.StaticImage = StaticImage
