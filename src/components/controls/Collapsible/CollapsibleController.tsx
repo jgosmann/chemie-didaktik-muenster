5dcc34c37f73628ee05749e2a636f5d85394a986
Jan Gosmann <jan@hyper-world.de>
2022-01-22 11:26:59 +0100

    Allow Collapsible to be expanded initiallly

--- src/components/controls/Collapsible/CollapsibleController.tsx
+++ src/components/controls/Collapsible/CollapsibleController.tsx
@@ -1,13 +1,15 @@
 import { useState, useCallback } from "react"
 
 export interface CollapsibleControllerProps {
+  initExpanded?: boolean
   render: (props: { isExpanded: boolean; onToggle: () => void }) => JSX.Element
 }
 
 const CollapsibleController = ({
+  initExpanded,
   render,
 }: CollapsibleControllerProps): JSX.Element => {
-  const [isExpanded, setIsExpanded] = useState(false)
+  const [isExpanded, setIsExpanded] = useState(!!initExpanded)
   const onToggle = useCallback(
     () => setIsExpanded(isExpanded => !isExpanded),
     []
